2068df30a8502e5b2a03a4bcb4216afe56451c66
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-13 09:02:58 +0100

    ajout exo6

--- index.php
+++ index.php
@@ -5,15 +5,13 @@
     <title>exercice 6 partie 5 php</title>
   </head>
   <body>
+    <a href="index.php?batiment=12&amp;salle=101">Lieu</a>
     <?php
-    $hautsDeFrance = array (
-      '59' => 'Nord',
-      '62' => 'Pas-de-Calais',
-      '80' => 'Somme',
-      '60' => 'Oise',
-      '02' => 'Aisne'
-    );
-    echo $hautsDeFrance['59'];
+    if (isset($_GET['batiment']) && ($_GET['salle'])) {
+    echo 'Nous sommes dans le batiment : ' . $_GET['batiment'] . ' Dans la salle : ' . $_GET['salle'];
+  } else {
+    echo 'Cliquez sur le lien pour afficher les informations !';
+  };
      ?>
   </body>
 </html>
